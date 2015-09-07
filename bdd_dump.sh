44facc249d080fc383be3b41d6f524853ba05ff1
Nicolas Rotier <nrotier@qosenergy.com>
2015-09-07 14:40:49 +0200

    mise en place du script d'export de la bdd

---
+++ bdd_dump.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+tmp=`pwd`
+
+
+echo -e "\n*** BDD DUMP ***"
+
+bddUser=$1
+bddPass=$2
+bddName=$3
+path_to_backup=$4
+branch=$5
+
+date=`date +"%d-%m-%Y-%T"`
+
+filename="$bddName"
+filename+="_$branch"
+filename+="_$date.sql"
+
+echo "creating =>  $filename"
+
+mysqldump -u$bddUser -p$bddPass $bddName > $path_to_backup/$filename
