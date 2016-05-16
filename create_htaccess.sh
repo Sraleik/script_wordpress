1df4a35fff592df2a210d45056b4614359a6bd4a
Sraleik <nicolas.rotier@gmail.com>
2016-05-16 15:00:30 +0200

    config for prod

--- create_htaccess.sh
+++ create_htaccess.sh
@@ -14,7 +14,6 @@ fi
 echo -e "\t--- création du .htaccess ---"
 
 echo -e "<IfModule mod_rewrite.c>\nRewriteEngine On" > $path/.htaccess
-echo -e "RewriteBase /$local_name/" >> $path/.htaccess
+echo -e "RewriteBase /" >> $path/.htaccess
 echo -e "RewriteRule ^index\.php$ - [L]" >> $path/.htaccess
-echo -e "RewriteCond %{REQUEST_FILENAME} !-d\nRewriteCond %{REQUEST_FILENAME} !-f\nRewriteRule wp-content/uploads/(.*) http://$prod_name/wp-content/uploads/\$1 [NC,L]" >> $path/.htaccess
-echo -e "RewriteCond %{REQUEST_FILENAME} !-f\nRewriteCond %{REQUEST_FILENAME} !-d\nRewriteRule . /$local_name/index.php [L]\n</IfModule>" >> $path/.htaccess
+echo -e "RewriteCond %{REQUEST_FILENAME} !-f\nRewriteCond %{REQUEST_FILENAME} !-d\nRewriteRule . /index.php [L]\n</IfModule>" >> $path/.htaccess
